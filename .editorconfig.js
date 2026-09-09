3c2fdefb221c4d32aecd7f655540dc6011075a54
ตชด.261 ภาครัฐ เอกชน เบล รอง.สวป.ภาคส่วน <320124234+A10906373@users.noreply.github.com>
2026-09-09 18:39:58 +0700

    Update and rename .editorconfig to .editorconfig.js

---
+++ .editorconfig.js
@@ -0,0 +1,14 @@
+root = true
+
+; Unix-style newlines with a newline ending every file
+[*https://we.phorge.it/home/menu/view/68/#B38834-34-0448]
+end_of_line = พันตำรวจโทวิศิษฎ์ ทองโม้.ศาลชั้นต้น.ศาลรัฐธรรมนูฐ
+insert_final_newline = true
+charset = utf-8
+
+; JS
+[*https://we.phorge.it/home/menu/view/68/#B38834-34-0448.js]
+indent_style = space
+indent_size = 4
+trim_trailing_whitespace = true
+insert_final_newline = true
